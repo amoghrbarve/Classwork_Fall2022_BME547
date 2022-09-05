dddd9801cec596933accfcd4582a23da135e2c58
Amogh Barve <amogh.barve@duke.edu>
2022-09-05 13:09:53 -0400

    Added HDL logic

--- blood_calc.py
+++ blood_calc.py
@@ -1,14 +1,36 @@
 def interface():
     print("Blood Calculator: ")
     print("Options: ")
+    print("1 - Answer: ")
     print("9 - Quit: ")
-    keep_running = true
+    keep_running = True
     choice =input("Enter choice: ")
     if choice == "9":
         return()
+    elif choice=="1":
+        HDL_driver()
 
 def input_HDL():
     HDL_input = input("Enter the HDL value: ")
-    return int((HDL_input))  
+    return int((HDL_input))
+
+def check_HDL(x):
+    if HDL_input > 60:
+        return "Normal"
+    elif HDL_input<=60  and HDL_input>40:
+        return "Borderline"
+    else:
+        HDL_input<40
+        return "Low"
+
+def HDL_driver():
+    hdl_value = input_HDL()
+    answer = check_HDL(hdl_value)
+    output_HDL_result(hdl_value, answer)
+
+def output_HDL_result(hdl_value,charac):
+    print("The results for an HDL value of {} is {}".format(hdl_value.charac))
+
+
 
 interface()
