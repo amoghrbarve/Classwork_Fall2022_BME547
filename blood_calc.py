898d8578942789e374302d404864156d13ec698b
Amogh Barve <amogh.barve@duke.edu>
2022-09-05 12:35:36 -0400

    Chanegd the code

--- blood_calc.py
+++ blood_calc.py
@@ -2,8 +2,10 @@ def interface():
     print("Blood Calculator: ")
     print("Options: ")
     print("9 - Quit: ")
+    keep_running = true
     choice =input("Enter choice: ")
     if choice = "9":
         return()
+    
 
 interface()
